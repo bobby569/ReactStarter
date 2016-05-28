5b8031966e9ce301c55e7d908d28921a3d26ebd6
Stephen Grider <ste.grider@gmail.com>
2016-05-28 11:50:49 -0700

    Improve here and there
    - update index.html & pkg
    - upgrade webpack
    - add more yarn cmds
    - add .babelrc and LICENSE.MD

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
-# ReduxSimpleStarter
+## Getting Started
 
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
-
-###Getting Started###
-
-There are two methods for getting started with this repo.
-
-####Familiar with Git?#####
-Checkout this repo, install dependencies, then start the gulp process with the following:
+### Checkout the repo
 
 ```
-	> git clone git@github.com:StephenGrider/ReduxSimpleStarter.git
-	> cd ReduxSimpleStarter
-	> npm install
-	> npm start
+$ git clone https://github.com/bobby569/react-starter.git
 ```
 
-####Not Familiar with Git?#####
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
+### Install dependencies
 
 ```
-	> npm install
-	> npm start
+$ cd react-starter
+$ npm install
+```
+
+### Build
+
+```
+$ npm run build
+```
+
+### Deploy to Github Page
+
+```
+$ npm run deploy
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,80 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HTMLWebapckPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+	'axios',
+	'lodash',
+	'react',
+	'react-dom',
+	'react-redux',
+	'react-router',
+	'redux'
+];
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+	entry: {
+		bundle: './src/index.js',
+		vendor: VENDOR_LIBS
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+		filename: '[name].[hash].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader'
+				})
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('style.css'),
+		new HTMLWebapckPlugin({
+			template: './index.html'
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			names: ['vendor', 'manifest']
+		})
+	],
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	devServer: {
+		historyApiFallback: true,
+		noInfo: true,
+		overlay: true
+	}
 };
+
+if (process.env.NODE_ENV === 'production') {
+	module.exports.devtool = '#source-map';
+	// http://vue-loader.vuejs.org/en/workflow/production.html
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"'
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true,
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.LoaderOptionsPlugin({
+			minimize: true
+		})
+	]);
+}
