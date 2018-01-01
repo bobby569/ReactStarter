62114468773cdd831e5539c374f75b7f36d25816
bobby569 <zhan2175@purdue.edu>
2018-01-01 00:21:58 -0500

    update package, upgrade webpack setting, add more npm feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -9,7 +10,8 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<App />
+	</Provider>,
+	document.querySelector('.container')
+);
